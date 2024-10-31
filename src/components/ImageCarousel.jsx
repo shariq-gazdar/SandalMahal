af1a936ff69fcc04634af3e3d6bab7c7976f5201
= <gazdarshariq0@gmail.com>
2024-10-31 21:34:21 +0500

    some more changes!!

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -4,7 +4,7 @@ import { Carousel } from "flowbite-react";
 function ImageCarousel({ images }) {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 my-5">
-      <Carousel>
+      <Carousel pauseOnHover={true} slideInterval={1500}>
         {images.map((image, index) => (
           <>
             <img key={index} src={image} alt=".../" className="w-full h-96" />
