e23ae9cdbee17fbc515ad6a1223f52f1d42ff6f7
= <gazdarshariq0@gmail.com>
2024-10-31 22:21:25 +0500

    things changed!!

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,9 +12,11 @@ function Card(props) {
         <p className="text-gray-400 mb-4">{props.gender}.</p>
         <div className="flex flex-col">
           <div className="text-lg font-bold">Rs .{props.price}</div>
-          <button className="bg-white text-black px-4 py-2 my-2 rounded-2xl font-medium hover:bg-yellow-500 hover:text-white transition hover:ring-2 ring-yellow-500 ">
-            Order Now!
-          </button>
+          <div className="   hover:ring-4 my-4 ring-yellow-400 rounded-full  transition-all h-fit  ">
+            <button className="bg-white text-black px-4 py-2  rounded-2xl font-medium transition-all w-full hover:bg-yellow-400 hover:text-white">
+              Order Now!
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -4,7 +4,12 @@ import { Carousel } from "flowbite-react";
 function ImageCarousel({ images }) {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 my-5">
-      <Carousel pauseOnHover={true} slideInterval={1500} draggable={true}>
+      <Carousel
+        pauseOnHover={true}
+        slideInterval={1500}
+        draggable={true}
+        indicators={false}
+      >
         {images.map((image, index) => (
           <>
             <img key={index} src={image} alt=".../" className="w-full h-96 " />
